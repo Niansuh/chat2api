38bbec7bc43bae610f11c3141fd8e54e8912a15f
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-06-21 19:56:22 +0500

    v1.3.3 fix error

--- chatgpt/chatFormat.py
+++ chatgpt/chatFormat.py
@@ -8,6 +8,7 @@ import uuid
 
 import pybase64
 import websockets
+from fastapi import HTTPException
 
 from api.files import get_file_content
 from api.models import model_system_fingerprint
@@ -47,6 +48,8 @@ async def format_not_stream_response(response, prompt_tokens, max_tokens, model)
         "completion_tokens": completion_tokens,
         "total_tokens": prompt_tokens + completion_tokens
     }
+    if not message.get("content"):
+        raise HTTPException(status_code=403, detail="No content in the message.")
     return {
         "id": chat_id,
         "object": "chat.completion",
@@ -118,6 +121,7 @@ async def stream_response(service, response, model, max_tokens):
 
     async for chunk in response:
         chunk = chunk.decode("utf-8")
+        # chunk = 'data: {"message": null, "conversation_id": "38b8bfcf-9912-45db-a48e-b62fb585c855", "error": "Our systems have detected unusual activity coming from your system. Please try again later."}'
         if end:
             yield "data: [DONE]\n\n"
             break
@@ -260,6 +264,12 @@ async def stream_response(service, response, model, max_tokens):
             else:
                 continue
         except Exception as e:
+            if chunk.startswith("data: "):
+                chunk_data = json.loads(chunk[6:])
+                if chunk_data.get("error"):
+                    logger.error(f"Error: {chunk_data.get('error')}")
+                    yield "data: [DONE]\n\n"
+                    break
             logger.error(f"Error: {chunk}, details: {str(e)}")
             continue
 
