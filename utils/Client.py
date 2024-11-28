79f7c01a2e67b51ec7cc3d5e05b26d52d51a5cbc
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-11-28 18:23:45 +0500

    v1.5.1

--- utils/Client.py
+++ utils/Client.py
@@ -4,38 +4,41 @@ from curl_cffi.requests import AsyncSession
 
 
 class Client:
-    def __init__(self, proxy=None, timeout=15, verify=True):
-        self.proxies = {
-            "http": proxy,
-            "https": proxy,
-        }
+    def __init__(self, proxy=None, timeout=15, verify=True, impersonate='safari15_3'):
+        self.proxies = {"http": proxy, "https": proxy}
         self.timeout = timeout
         self.verify = verify
-        self.impersonate = random.choice(["safari15_3"])
-        self.session = AsyncSession(proxies=self.proxies, timeout=self.timeout, verify=self.verify)
-        self.session2 = AsyncSession(proxies=self.proxies, timeout=self.timeout, verify=self.verify)
+
+        self.impersonate = impersonate
+        # impersonate=self.impersonate
+
+        # self.ja3 = ""
+        # self.akamai = ""
+        # ja3=self.ja3, akamai=self.akamai
+        self.session = AsyncSession(proxies=self.proxies, timeout=self.timeout, impersonate=self.impersonate, verify=self.verify)
+        self.session2 = AsyncSession(proxies=self.proxies, timeout=self.timeout, impersonate=self.impersonate, verify=self.verify)
 
     async def post(self, *args, **kwargs):
-        r = await self.session.post(*args, impersonate=self.impersonate, **kwargs)
+        r = await self.session.post(*args, **kwargs)
         return r
 
     async def post_stream(self, *args, headers=None, cookies=None, **kwargs):
         if self.session:
             headers = headers or self.session.headers
             cookies = cookies or self.session.cookies
-        r = await self.session2.post(*args, headers=headers, cookies=cookies, impersonate=self.impersonate, **kwargs)
+        r = await self.session2.post(*args, headers=headers, cookies=cookies, **kwargs)
         return r
 
     async def get(self, *args, **kwargs):
-        r = await self.session.get(*args, impersonate=self.impersonate, **kwargs)
+        r = await self.session.get(*args, **kwargs)
         return r
 
     async def request(self, *args, **kwargs):
-        r = await self.session.request(*args, impersonate=self.impersonate, **kwargs)
+        r = await self.session.request(*args, **kwargs)
         return r
 
     async def put(self, *args, **kwargs):
-        r = await self.session.put(*args, impersonate=self.impersonate, **kwargs)
+        r = await self.session.put(*args, **kwargs)
         return r
 
     async def close(self):
